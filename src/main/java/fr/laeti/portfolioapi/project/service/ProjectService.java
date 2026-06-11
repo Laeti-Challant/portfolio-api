292ec183d08dfe207e8196931ea8f373b48e7808
Laeti-Challant <l.challant@gmail.com>
2026-06-11 10:28:39 +0200

    fix: update cacheable in projectService

--- src/main/java/fr/laeti/portfolioapi/project/service/ProjectService.java
+++ src/main/java/fr/laeti/portfolioapi/project/service/ProjectService.java
@@ -40,7 +40,7 @@ public class ProjectService {
 
     // ─── Détail d'un projet ───────────────────────────────────────────────────
 
-    @Cacheable(value = "projectDetail", key = "#a0")
+    @Cacheable(value = "projectDetail", key = "#name")
     public ProjectDetailDTO getProjectByName(String name) {
         log.info("Chargement du détail du projet : {}", name);
 
